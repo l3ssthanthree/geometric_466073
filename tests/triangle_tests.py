44c7eb107b0ba4354b581fb0fadd3673bcfe07fe
Daniil Kazanovich <466073@niuitmo.ru>
2024-12-12 03:23:21 +0300

    Update triangle_tests.py

--- tests/triangle_tests.py
+++ tests/triangle_tests.py
@@ -1,12 +1,6 @@
-import sys
-import os
 import unittest
 import math
-
-# Добавляем путь к модулю triangle.
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), 'C:/Users/lucky/geometric_lib_466073')))
-
-from triangle import area, perimeter
+from ..triangle import area, perimeter
 
 class TriangleTestCase(unittest.TestCase):
     """Тесты для функций расчета площади и периметра треугольника."""
@@ -28,21 +22,21 @@ class TriangleTestCase(unittest.TestCase):
         self.assertEqual(result, expected_area, msg="Area for one side 0 should be 0.")
 
     def test_perimeter_positive(self):
-        """Тест: положительные основания и высота."""
+        """Тест: положительные стороны треугольника."""
         a = 3
         b = 4
         c = math.sqrt(a**2 + b**2)  # Гипотенуза
         expected_perimeter = a + b + c
-        result = perimeter(a, b)
+        result = perimeter(a, b, c) # Исправлен вызов функции perimeter
         self.assertAlmostEqual(result, expected_perimeter, places=5, msg="Perimeter calculation is incorrect.")
 
     def test_perimeter_zero(self):
-        """Тест: одно основание равно 0."""
+        """Тест: одна сторона равна 0."""
         a = 0
         b = 4
         c = math.sqrt(a**2 + b**2)  # Гипотенуза
         expected_perimeter = b + c  # Периметр при a = 0
-        result = perimeter(a, b)
+        result = perimeter(a, b, c) # Исправлен вызов функции perimeter
         self.assertAlmostEqual(result, expected_perimeter, places=5, msg="Perimeter for side a = 0 should be correct.")
 
 if __name__ == "__main__":
